ddbef3b7610d2a9eafe6871fa3c618bbd58d7b33
killivion <dievteilederexodia@gmail.com>
2025-03-12 12:49:28 +0100

    More Augmentations

--- hyperparameters.py
+++ hyperparameters.py
@@ -50,6 +50,11 @@ SIGCWGAN_CONFIGS = dict(
         sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
         sig_config_future=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
     ),
+    correlated_Blackscholes=SigCWGANConfig(
+        mc_size=1000,
+        sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
+        sig_config_future=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
+    ),
     Heston=SigCWGANConfig(
         mc_size=1000,
         sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
