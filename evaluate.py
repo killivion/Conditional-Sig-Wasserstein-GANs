031c5fcc5042ee378da4b372d610a783f408feda
killivion <dievteilederexodia@gmail.com>
2025-03-12 16:09:52 +0100

    Split Update

--- evaluate.py
+++ evaluate.py
@@ -197,7 +197,7 @@ def evaluate_benchmarks(algos, base_dir, datasets, use_cuda=False):
                     )
                     # add relevant parameters used during training to the experiment summary
                     experiment_summary = complete_experiment_summary(dataset_dir, experiment_dir, experiment_summary)
-                    df = df.append(experiment_summary, ignore_index=True, )
+                    df = df.append(experiment_summary, ignore_index=True, ) #pd.concat([df, experiment_summary], ignore_index=True, )
             df_dst_path = os.path.join(experiment_path, 'summary.csv')
             print(f'Saving summary to {df_dst_path} ...')
             df.to_csv(df_dst_path, decimal=',', sep=';', float_format='%.5f', index=False)
@@ -209,7 +209,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Turn cuda off / on during evalution.')
     parser.add_argument('-base_dir', default='./numerical_results', type=str)
     parser.add_argument('-use_cuda', action='store_true')
-    parser.add_argument('-datasets', default=['ARCH'], nargs="+")  # , 'STOCKS', 'ECG', 'VAR',
-    parser.add_argument('-algos', default=['SigCWGAN', 'GMMN', 'RCGAN', 'TimeGAN', 'RCWGAN', ], nargs="+")
+    parser.add_argument('-datasets', default=['correlated_Blackscholes'], nargs="+")  # , 'STOCKS', 'ECG', 'VAR',
+    parser.add_argument('-algos', default=['SigCWGAN'], nargs="+") #, 'GMMN', 'RCGAN', 'TimeGAN', 'RCWGAN',
     args = parser.parse_args()
     evaluate_benchmarks(base_dir=args.base_dir, use_cuda=args.use_cuda, datasets=args.datasets, algos=args.algos)
