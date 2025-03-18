c7ec39112a8aac235e804615ab2138e3e7d5b06e
killivion <dievteilederexodia@gmail.com>
2025-03-18 15:49:48 +0100

    Generate Testing

--- generate.py
+++ generate.py
@@ -37,8 +37,12 @@ def generate_from_generator(spec, experiment_dir, dataset, use_cuda=True):
     with torch.no_grad():
         idx = torch.randint(0, x_past.shape[0], (1,)).item()
         x_past_sample = x_past[idx:idx + args.sig_q]
+        print(x_past_sample)
         _x_past = x_past_sample.clone()
+        print(_x_past)
         x_fake_future = G.sample(q, _x_past)
+        print(x_fake_future)
+
 
     return x_fake_future
 
